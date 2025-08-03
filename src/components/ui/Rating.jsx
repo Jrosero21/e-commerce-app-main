5bf7fd8d99129d146d011c093a584c9a05747b6f
Jrosero21 <150304100+Jrosero21@users.noreply.github.com>
2025-08-03 04:33:01 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/ui/Rating.jsx
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+const Rating = ({ rating }) => {
+  return (
+    <div className="book__ratings">
+        <div className="book__ratings">
+        {new Array(Math.floor(rating)).fill(0).map((_, index) => (
+          <FontAwesomeIcon icon="fa-solid fa-star" style={{ color: "#FFD43B" }} key={index} />
+        ))}
+        {Number.isInteger(rating) ? (
+          ""
+        ) : (
+          <FontAwesomeIcon icon="fa-solid fa-star-half-stroke" style={{ color: "#FFD43B" }} />
+        )}
+      </div>
+      
+    </div>
+  )
+}
+
+export default Rating
